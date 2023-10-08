541deade43ddefa6a1582da9ff48fd45d98f94ce
Md Noble <mdnoble.wd@gmail.com>
2023-10-08 15:18:00 +0600

    added navbar

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,12 +1,65 @@
 import { NavLink } from "react-router-dom";
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
-    return (
-        <div>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/register">Register</NavLink>
+  const navLinks = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-black underline" : ""
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/login"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-black underline" : ""
+          }
+        >
+          Login
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/register"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-black underline" : ""
+          }
+        >
+          Register
+        </NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-[#ff6b35]">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden text-xl">
+            <FaBars></FaBars>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-200 rounded-box w-52 font-semibold text-lg text-[#ff6b35]"
+          >
+            {navLinks}
+          </ul>
         </div>
-    );
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu-horizontal px-1 font-semibold text-xl text-white gap-10">
+            {navLinks}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
