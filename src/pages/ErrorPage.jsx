11e09efba857782b241df178366fc2ab2db33e5f
Md Noble <mdnoble.wd@gmail.com>
2023-10-08 22:22:54 +0600

    added an error 404 page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div className="h-screen flex justify-center items-center text-center text-2xl lg:text-4xl">
+      <div>
+        <h1>Oops! You seem to be lost!!</h1>
+        <br />
+        <p>Go back to Career Nook Home</p>
+        <Link to="/">
+            <button className="btn btn-outline mt-6">Home</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,11 +3,13 @@ import Root from "../layouts/Root";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: "/",
